2bff7eb549d8d152f180a564b903ff0d4b78c820
Rudy Matos <rudymatos.dev@gmail.com>
2019-01-24 17:29:40 -0500

    fixing issue with request parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,10 +4,10 @@ import Vapor
 public func routes(_ router: Router) throws {
     router.get("cpr-callback-functions/callback/twitter"){ request -> Response in
         print(request.query)
-        guard let oauthToken = try? request.query.get(String.self, at: "oauth_token"), let oauthTokenSecret = try? request.query.get(String.self, at: "oauth_token_secret"), let oauthCallbackConfirmed = try? request.query.get(String.self, at: "oauth_callback_confirmed") else {
+        guard let oauthToken = try? request.query.get(String.self, at: "oauth_token"), let oauthVerifier = try? request.query.get(String.self, at: "oauth_verifier") else {
             return request.redirect(to: "OAuthTesting://oauth-swift/twitter")
         }
-        let queryString = "oauth_token=\(oauthToken)&oauth_token_secret=\(oauthTokenSecret)&oauth_callback_confirmed=\(oauthCallbackConfirmed)"
+        let queryString = "oauth_token=\(oauthToken)&oauth_verifier=\(oauthVerifier)"
         return request.redirect(to: "OAuthTesting://oauth-swift/twitter?\(queryString)")
     }
 }
