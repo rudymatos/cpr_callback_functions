9fa17715da303c527da0493c14a3a176fed1bd18
Rudy Matos <rudymatos.dev@gmail.com>
2019-01-30 12:03:41 -0500

    adding pinterest router

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,7 @@
 import Vapor
 
 public func routes(_ router: Router) throws {
+    
     router.get("cpr-callback-functions/callback/twitter"){ request -> Response in
         print(request.query)
         guard let oauthToken = try? request.query.get(String.self, at: "oauth_token"), let oauthVerifier = try? request.query.get(String.self, at: "oauth_verifier") else {
@@ -9,4 +10,14 @@ public func routes(_ router: Router) throws {
         let queryString = "oauth_token=\(oauthToken)&oauth_verifier=\(oauthVerifier)"
         return request.redirect(to: "OAuthTesting://oauth-swift/twitter?\(queryString)")
     }
+    
+    router.get("cpr-callback-functions/callback/pinterest"){ request -> Response in
+        print(request.query)
+        guard let oauthToken = try? request.query.get(String.self, at: "oauth_token"), let oauthVerifier = try? request.query.get(String.self, at: "oauth_verifier") else {
+            return request.redirect(to: "OAuthTesting://oauth-swift/pinterest")
+        }
+        let queryString = "oauth_token=\(oauthToken)&oauth_verifier=\(oauthVerifier)"
+        return request.redirect(to: "OAuthTesting://oauth-swift/pinterest?\(queryString)")
+    }
+    
 }
