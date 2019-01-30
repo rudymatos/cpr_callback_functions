b9152b716d3154817515938ab6210ca9aebe4cf0
Rudy Matos <rudymatos.dev@gmail.com>
2019-01-30 14:18:01 -0500

    getting state and code from pinterest callback

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,10 +13,12 @@ public func routes(_ router: Router) throws {
     
     router.get("cpr-callback-functions/callback/pinterest"){ request -> Response in
         print(request.query)
-        guard let oauthToken = try? request.query.get(String.self, at: "oauth_token"), let oauthVerifier = try? request.query.get(String.self, at: "oauth_verifier") else {
+        guard let state = try? request.query.get(String.self, at: "state"), let code = try? request.query.get(String.self, at: "code") else {
             return request.redirect(to: "OAuthTesting://oauth-swift/pinterest")
         }
-        let queryString = "oauth_token=\(oauthToken)&oauth_verifier=\(oauthVerifier)"
+        print(state)
+        print(code)
+        let queryString = "state=\(state)&code=\(code)"
         return request.redirect(to: "OAuthTesting://oauth-swift/pinterest?\(queryString)")
     }
     
