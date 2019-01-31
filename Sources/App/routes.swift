25b85ca17a18572816f3b10c7443d4c1276e4099
Rudy Matos <rudymatos.dev@gmail.com>
2019-01-31 15:34:00 -0500

    redirecting to CPRMobileApp instead

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,23 +5,23 @@ public func routes(_ router: Router) throws {
     router.get("cpr-callback-functions/callback/twitter"){ request -> Response in
         print("Entering to twitter callback with params: \(request.query)")
         guard let oauthToken = try? request.query.get(String.self, at: "oauth_token"), let oauthVerifier = try? request.query.get(String.self, at: "oauth_verifier") else {
-            print("redirecting to OAuthTesting://oauth-swift/twitter")
-            return request.redirect(to: "OAuthTesting://oauth-swift/twitter")
+            print("redirecting to CPRMobileApp://oauth-swift/twitter")
+            return request.redirect(to: "CPRMobileApp://oauth-swift/twitter")
         }
         let queryString = "oauth_token=\(oauthToken)&oauth_verifier=\(oauthVerifier)"
-        print("redirecting to OAuthTesting://oauth-swift/twitter?\(queryString)")
-        return request.redirect(to: "OAuthTesting://oauth-swift/twitter?\(queryString)")
+        print("redirecting to CPRMobileApp://oauth-swift/twitter?\(queryString)")
+        return request.redirect(to: "CPRMobileApp://oauth-swift/twitter?\(queryString)")
     }
     
     router.get("cpr-callback-functions/callback/pinterest"){ request -> Response in
         print("Entering to pinterest callback with params: \(request.http.url.query)")
         guard let queryString = request.http.url.query?.replacingOccurrences(of: "state,", with: "state=").replacingOccurrences(of: "code,", with: "code="),
             queryString.split(separator:"&").count >= 2 else {
-                print("redirecting to OAuthTesting://oauth-swift/pinterest")
-            return request.redirect(to: "OAuthTesting://oauth-swift/pinterest")
+                print("redirecting to CPRMobileApp://oauth-swift/pinterest")
+            return request.redirect(to: "CPRMobileApp://oauth-swift/pinterest")
         }
-        print("redirecting to OAuthTesting://oauth-swift/pinterest?\(queryString)")
-        return request.redirect(to: "OAuthTesting://oauth-swift/pinterest?\(queryString)")
+        print("redirecting to CPRMobileApp://oauth-swift/pinterest?\(queryString)")
+        return request.redirect(to: "CPRMobileApp://oauth-swift/pinterest?\(queryString)")
     }
     
 }
